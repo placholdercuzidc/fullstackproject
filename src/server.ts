b725e7fce5ce9fc25a72555f141b2c36733ef90d
Brandon Chen <brandonchenbc@berkeley.edu>
2026-04-29 19:30:40 -0700

    first com

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'On55en1509240924!',
+  password: '***',
   database: 'restaurant_db'
 });
 
